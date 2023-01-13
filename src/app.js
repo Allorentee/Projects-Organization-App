10df70a99049613c4190882f624fd53eadf2a24d
AlvaroLlorente <Allorentee@gmail.com>
2023-01-13 10:26:11 +0100

    Changes on rutos to deploy

--- src/app.js
+++ src/app.js
@@ -6,4 +6,7 @@ export const app = express();
 //Middleware
 //permite cada vez que envie un dato json el servidor lo puede interpretar y guardar en un req.body
 app.use(express.json());
-app.use(route);
+app.get('/', (_req, res) => {
+  res.send('API Express / projects or tasks').end();
+});
+app.use('/', route);
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -3,10 +3,10 @@ import { ProjectsController } from '../controller/projects.controller.js';
 export const route = Router();
 const controller = new ProjectsController();
 
-route.get('/projects', controller.getProjects);
-route.post('/projects', controller.createProjects);
+route.get('projects', controller.getProjects);
+route.post('projects', controller.createProjects);
 //actualizar.
-route.patch('/projects', controller.updateProject);
-route.put('/projects/:id', controller.putProject);
-route.delete('/projects', controller.deleteProject);
-route.get('/projectos/:id');
+route.patch('projects', controller.updateProject);
+route.put('projects/:id', controller.putProject);
+route.delete('projects', controller.deleteProject);
+route.get('projectos/:id');
