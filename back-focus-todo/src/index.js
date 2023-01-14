ec077ede64b59e5a7e4853cf15134e65842c58af
AlvaroLlorente <Allorentee@gmail.com>
2023-01-14 11:42:43 +0100

    Updater Index

--- back-focus-todo/src/index.js
+++ back-focus-todo/src/index.js
@@ -1,9 +1,26 @@
 import { sequelize } from './database/database.js';
 import { app } from './app.js';
+import http from 'http';
+
+const port = process.env.PORT || 3200;
+const server = http.createServer(app);
+server.on('listening', () => {
+  const addr = server.address();
+  if (addr === null) return;
+  if (typeof addr === 'string') {
+    bind = 'pipe' + addr;
+  } else {
+    bind =
+      addr.address === '::'
+        ? `http://localhost:${addr.port}`
+        : `port ${addr?.port}`;
+  }
+  console.log(`Listening ${bind}`);
+});
 
 async function main() {
   try {
-    await sequelize.sync({ force: false });
+    await sequelize.sync({ force: true });
     //Este ync tiene varias formas de trabajar,
     console.log('Connection has been established successfully.');
     app.listen(3000);
