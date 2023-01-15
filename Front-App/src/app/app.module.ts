58ca591938c5a20ff33c4684f68ff164cbb5f3bb
AlvaroLlorente <Allorentee@gmail.com>
2023-01-15 16:06:34 +0100

    Add header component

--- Front-App/src/app/app.module.ts
+++ Front-App/src/app/app.module.ts
@@ -1,22 +1,18 @@
+import { HeaderComponent } from './components/header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  //Componentes directivas y pipes
+  declarations: [AppComponent,HeaderComponent],
+  //Otros modulos
+  imports: [BrowserModule, AppRoutingModule],
+  //Exportados hace que los elementos declarados se puedan exportar a otros modulos.
+  //Los elementos sobre los que este modulo va a actuar como proveedor.
   providers: [],
-  bootstrap: [AppComponent]
+  //En el modulo principal se tiene que declarar el componte exportado como componente principal.
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
