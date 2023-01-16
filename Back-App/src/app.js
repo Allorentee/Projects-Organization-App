9f62457f6c0e5164321f4b46136b54f89bc37a97
AlvaroLlorente <Allorentee@gmail.com>
2023-01-16 14:05:52 +0100

    ADD backend Cors options

--- Back-App/src/app.js
+++ Back-App/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import { corsOptions } from './cors/corsOptions.js';
 import { projectsRoutes } from './routes/projects.routes.js';
 import { tasksRoutes } from './routes/task.routes.js';
 
 export const app = express();
 
+app.use(cors(corsOptions));
 //Middleware
 //permite cada vez que envie un dato json el servidor lo puede interpretar y guardar en un req.body
 app.use(express.json());
