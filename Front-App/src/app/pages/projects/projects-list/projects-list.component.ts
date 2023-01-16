726d12cab9e906c3f6e825dce1f00247010d37cc
AlvaroLlorente <Allorentee@gmail.com>
2023-01-16 17:15:20 +0100

    Update project list component

--- Front-App/src/app/pages/projects/projects-list/projects-list.component.ts
+++ Front-App/src/app/pages/projects/projects-list/projects-list.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { Project } from 'src/app/services/interface/project';
+import { ProjectService } from 'src/app/services/projects.service';
 
 @Component({
   selector: 'app-projects-list',
   templateUrl: './projects-list.component.html',
   styleUrls: ['./projects-list.component.scss'],
 })
-export class ProjectsListComponent {}
+export class ProjectsListComponent {
+  projects!: Project[];
+  error!: Error;
+  loading: boolean;
+
+  constructor(public projectService: ProjectService) {
+    this.loading = true;
+    this.projectService.getAllProjects().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.projects = data;
+        this.loading = false;
+        console.log(this.projects);
+      },
+      error: (error) => (console.log(this.projects), (this.error = error)),
+    });
+  }
+
+  ngOnInit() {
+    this.projects = [];
+    console.log(this.projects);
+  }
+}
