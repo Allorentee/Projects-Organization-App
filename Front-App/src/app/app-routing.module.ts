4d8eb599c1a6fa4aecded8cc50bbbb1929f8fbb8
AlvaroLlorente <Allorentee@gmail.com>
2023-01-16 10:47:36 +0100

    ADD page routes

--- Front-App/src/app/app-routing.module.ts
+++ Front-App/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  {
+    path: 'Projects',
+    loadChildren: () =>
+      import('./pages/projects/projects/projects.module').then(
+        (m) => m.ProjectsModule
+      ),
+  },
+  {
+    path: 'Tasks',
+    loadChildren: () =>
+      import('./pages/tasks/tasks.module').then((m) => m.TasksModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-App/src/app/app.module.ts
+++ Front-App/src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { ProjectsComponent } from './pages/projects/projects/projects.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects/projects/projects.component';
-import { HomeComponent } from './pages/home/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   //Componentes directivas y pipes
-  declarations: [AppComponent,HeaderComponent, ProjectsComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProjectsComponent,
+    HomeComponent,
+  ],
   //Otros modulos
   imports: [BrowserModule, AppRoutingModule],
   //Exportados hace que los elementos declarados se puedan exportar a otros modulos.
