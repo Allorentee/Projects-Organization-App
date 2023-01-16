f9e0d494813f35bccd95b32633968800cc085046
AlvaroLlorente <Allorentee@gmail.com>
2023-01-16 15:27:03 +0100

    FIX backend deploy

--- Back-App/src/cors/setCors.js
+++ Back-App/src/cors/setCors.js
@@ -1,5 +1,3 @@
-import { Request, Response, NextFunction } from 'express';
-
 export const setCors = (req, res, next) => {
   const origin = req.header('Origin') || '*';
   res.setHeader('Access-Control-Allow-Origin', origin);
