26a39e4cd4215e560dd91c9a03eaa199133c7f49
AlvaroLlorente <Allorentee@gmail.com>
2023-01-17 14:29:29 +0100

    FIX deploy

--- Front-App/src/app/app.module.ts
+++ Front-App/src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './shared/core.module';
 
 @NgModule({
   //Componentes directivas y pipes
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent],
   //Otros modulos
   imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
   //Exportados hace que los elementos declarados se puedan exportar a otros modulos.
